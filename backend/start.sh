bcfe024015eb45815e7e7c896700498c24ccf24b
Joel Siby <126755343+joelsiby02@users.noreply.github.com>
2026-06-24 01:23:57 +0530

    Render limit exceeding deploy puhsing Api on render

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Start FastAPI server in the background
-uvicorn main:app --host 0.0.0.0 --port 8082 &
+uvicorn main:app --host 0.0.0.0 --port 8082
 
 # Start the LiveKit agent worker
 python main.py start &
